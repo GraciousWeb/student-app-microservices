20b222f5c392ed3f593e833583f0a5e53fc35f6b
Grace Onuoha <126955450+GraciousWeb@users.noreply.github.com>
2023-10-07 15:12:02 +0100

    Add files via upload
    Initial Commit

---
+++ src/test/java/com/app/studentappmicroservices/StudentAppMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.studentappmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
